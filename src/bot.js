7299dfdb29312090b262122eb63700b4ad456665
Sean <Seancraft20066@gmail.com>
2022-08-15 00:18:29 -0400

    Fixed!

--- src/bot.js
+++ src/bot.js
@@ -39,13 +39,13 @@ client.handleComponents();
 
 
 // the bot emojis handler
-client.Check = emojis.Check
-client.Cross = emojis.Cross
-client.Owner = emojis.Owner
-client.Developer = emojis.Developer
-client.Designer = emojis.Designer
-client.Artist = emojis.Artist
-
+client.Check = emojis.Check;
+client.Cross = emojis.Cross;
+client.Owner = emojis.Owner;
+client.Developer = emojis.Developer;
+client.Designer = emojis.Designer;
+client.Artist = emojis.Artist;
+client.Invite = emojis.Invite;
 
 // the bot loggin 
 client.login(Token);
