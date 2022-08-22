899f9c39fc7ccccd643a0ebaf07aae514c4d287c
Sean Gray <seancraft20066@gmail.com>
2022-08-22 02:29:35 -0400

    Minor updates

--- src/events/client/presenceUpdate.js
+++ src/events/client/presenceUpdate.js
@@ -3,23 +3,23 @@ const config = require("../../../config.json");
 module.exports = {
   name: "presenceUpdate",
   async execute(oldPresence, newPresence, client) {
-      try {
-        switch (newPresence.status) {
-          case "online":
-            client.user.setStatus("online");
-            break;
-          case "idle":
-            client.user.setStatus("idle");
-            break;
-          case "dnd":
-            client.user.setStatus("dnd");
-            break;
-          default:
-            console.log("Owner has gone invisible mode or bot has not been updated");
-        }
-      } catch (err) {
-        console.log(err);
+    try {
+      switch (newPresence.status) {
+        case "online":
+          client.user.setStatus("online");
+          break;
+        case "idle":
+          client.user.setStatus("idle");
+          break;
+        case "dnd":
+          client.user.setStatus("dnd");
+          break;
+        default:
+          return;
       }
+    } catch (err) {
+      console.log(err);
+    }
 
   },
 };
